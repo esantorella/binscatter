e883deed15580880a46e2e0a779c602364210f16
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-11-05 10:33:25 -0500

    No longer taking column names as arguments; recenter_x and recenter_y both options

--- binscatter.py
+++ binscatter.py
@@ -8,37 +8,53 @@ import matplotlib
 from matplotlib import pyplot as plt
 from sklearn import linear_model
 import warnings
+from scipy import sparse as sps
 
 
-def get_binscatter_objects(data, y, x, controls, n_bins, recenter_x):
+def get_binscatter_objects(y: np.ndarray, x: np.ndarray, controls, n_bins, recenter_x,
+                           recenter_y):
     """
     Returns x_means, y_means, coefficients.
-    :param data:
     :param y:
     :param x:
-    :param controls:
+    :param controls: None, numpy array, or sparse matrix
     :param n_bins:
+    :param recenter_x:
+    :param recenter_y:
     :return:
     """
     # Check if data is sorted
-    if np.any(np.diff(data[x]) < 0):
-        warnings.warn('Data was not sorted. Going to sort.')
-        data = data.sort_values(x).reset_index(drop=True)
+
     if controls is None:
-        x_data = data[x]
-        y_data = data[y]
+        if np.any(np.diff(x) < 0):
+            argsort = np.argsort(x)
+            x = x[argsort]
+            y = y[argsort]
+        x_data = x
+        y_data = y
     else:
         # Residualize
-        assert set(controls).issubset(set(data.columns))
-        demeaning_y_reg = linear_model.LinearRegression().fit(data[controls], data[y])
-        y_data = data[y] - demeaning_y_reg.predict(data[controls]) + data[y].mean()
-        demeaning_x_reg = linear_model.LinearRegression().fit(data[controls], data[x])
-        x_data = data[x] - demeaning_x_reg.predict(data[controls])
-        if recenter_x:
-            x_data += data[x].mean()
+        if controls.ndim == 1:
+            controls = controls[:, None]
 
-    reg = linear_model.LinearRegression().fit(x_data[:, None], y_data)
-    bin_edges = np.linspace(0, len(data), n_bins + 1).astype(int)
+        demeaning_y_reg = linear_model.LinearRegression().fit(controls, y)
+        y_data = y - demeaning_y_reg.predict(controls)
+
+        demeaning_x_reg = linear_model.LinearRegression().fit(controls, x)
+        x_data = x - demeaning_x_reg.predict(controls)
+        argsort = np.argsort(x_data)
+        x_data = x_data[argsort]
+        y_data = y_data[argsort]
+
+        if recenter_y:
+            y_data += np.mean(y)
+        if recenter_x:
+            x_data += np.mean(x)
+
+    if x_data.ndim == 1:
+        x_data = x_data[:, None]
+    reg = linear_model.LinearRegression().fit(x_data, y_data)
+    bin_edges = np.linspace(0, len(y), n_bins + 1).astype(int)
     assert len(bin_edges) == n_bins + 1
     bins = [slice(bin_edges[i], bin_edges[i + 1]) for i in range(len(bin_edges) - 1)]
     assert len(bins) == n_bins
@@ -48,49 +64,67 @@ def get_binscatter_objects(data, y, x, controls, n_bins, recenter_x):
     return x_means, y_means, reg.intercept_, reg.coef_[0]
 
 
-def binscatter(self, data, y, x, controls=None, n_bins=20,
-               line_kwargs=None, scatter_kwargs=None, recenter_x=False):
+def binscatter(self, y: np.ndarray, x: np.ndarray, controls=None, n_bins=20,
+               line_kwargs=None, scatter_kwargs=None, recenter_x=False,
+               recenter_y=True):
     """
 
     :param self: matplotlib.axes.Axes object
-    :param data: pandas DataFrame
-    :param y: string, referring to a column in data
-    :param x:
-    :param controls: list of strings; all should be columns in data
+    :param y: 1d numpy array or pandas series
+    :param x: 1d numpy array or Pandas Series
+    :param controls: numpy array or sparse matrix
     :param n_bins: int, default 20
     :param line_kwargs: dict
     :param scatter_kwargs: dict
-    :param recenter_x: If true, recenter x the same way y was recentered
+    :param recenter_y: If true, recenter y-tilde so its mean is the mean of y
+    :param recenter_x: If true, recenter y-tilde so its mean is the mean of y
     :return:
     """
     if line_kwargs is None:
         line_kwargs = {}
     if scatter_kwargs is None:
         scatter_kwargs = {}
+    if controls is not None:
+        if isinstance(controls, pd.SparseDataFrame) or isinstance(controls, pd.SparseSeries):
+            controls = controls.to_coo()
+        elif isinstance(controls, pd.DataFrame) or isinstance(controls, pd.Series):
+            controls = controls.values
+        assert isinstance(controls, np.ndarray) or sps.issparse(controls)
 
-    x_means, y_means, intercept, coef = get_binscatter_objects(data, y, x, controls, n_bins,
-                                                               recenter_x)
+    x_means, y_means, intercept, coef = get_binscatter_objects(np.asarray(y), np.asarray(x),
+                                                               controls, n_bins, recenter_x,
+                                                               recenter_y)
 
     self.scatter(x_means, y_means, **scatter_kwargs)
     x_range = np.array(self.get_xlim())
-    self.plot(x_range, intercept + x_range * coef, label='beta=' + str(coef), **line_kwargs)
-    return self
+    self.plot(x_range, intercept + x_range * coef, label='beta=' + str(round(coef, 3)), **line_kwargs)
+    # If series were passed, might be able to label
+    try:
+        self.set_xlabel(x.name)
+    except AttributeError:
+        pass
+    try:
+        self.set_ylabel(y.name)
+    except AttributeError:
+        pass
+    return coef
 
 matplotlib.axes.Axes.binscatter = binscatter
 
-if __name__ == '__main__':
+
+def main():
     n_obs = 1000
     data = pd.DataFrame({'experience': np.random.poisson(4, n_obs) + 1})
     data['tenure'] = data['experience'] + np.random.normal(0, 1, n_obs)
     data['wage'] = data['experience'] + data['tenure'] + np.random.normal(0, 1, n_obs)
 
-    fig, axes = plt.subplots(2)
-    axes[0].binscatter(data, 'wage', 'tenure')
+    fig, axes = plt.subplots(1, 2)
+    axes[0].binscatter(data['wage'], data['tenure'])
     axes[0].legend()
     axes[0].set_ylabel('Wage')
     axes[0].set_ylabel('Tenure')
     axes[0].set_title('No controls')
-    axes[1].binscatter(data, 'wage', 'tenure', controls=['experience'])
+    axes[1].binscatter(data['wage'], data['tenure'], controls=data['experience'], recenter=False)
     axes[1].set_xlabel('Tenure (residualized)')
     axes[1].set_ylabel('Wage (residualized, recentered)')
     axes[1].legend()
@@ -98,17 +132,21 @@ if __name__ == '__main__':
     plt.savefig('test')
     plt.close('all')
 
-    # Make x more interpretable
-    fig, axes = plt.subplots(2, sharex=True, sharey=True)
-    axes[0].binscatter(data, 'wage', 'tenure')
+    # Make y more interpretable
+    fig, axes = plt.subplots(1, 2, sharey=True)
+    axes[0].binscatter(data['wage'], data['tenure'])
     axes[0].legend()
     axes[0].set_ylabel('Wage')
     axes[0].set_ylabel('Tenure')
     axes[0].set_title('No controls')
-    axes[1].binscatter(data, 'wage', 'tenure', controls=['experience'], recenter_x=True)
-    axes[1].set_xlabel('Tenure (residualized, recentered)')
-    axes[1].set_ylabel('Wage (residualized, recentered)')
+    axes[1].binscatter(data['wage'], data['tenure'], controls=data['experience'])
+    # axes[1].set_xlabel('Tenure (residualized, recentered)')
+    # axes[1].set_ylabel('Wage (residualized, recentered)')
     axes[1].legend()
     axes[1].set_title('Controlling for experience')
     plt.savefig('test2')
     plt.close('all')
+    return
+
+if __name__ == '__main__':
+    main()
