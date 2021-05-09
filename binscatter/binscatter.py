696a7eb7a6be0696f34c649f438c18755326f6d3
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2021-05-09 18:49:32 -0400

    Cleanup, tests, and fit_reg argument (#6)
    * Added pre-commit, ran black
    
    * Corrected argument order and labeling in examples
    
    * Added tests -currently failing because sparse data frames are no longer supported
    
    * Parametrized texts and removed trivial dependencies
    
    * Optional fit_reg argument

--- binscatter/binscatter.py
+++ binscatter/binscatter.py
@@ -1,10 +1,17 @@
 """Monkey-patch Matplotlib to add an 'ax.binscatter' method."""
+import warnings
 from typing import Dict, Iterable, List, Optional, Tuple
 
 import matplotlib
 import numpy as np
 import numpy.typing as npt
 from sklearn import linear_model
+
+
+def _get_bins(n_elements: int, n_bins: int) -> List[slice]:
+    bin_edges = np.linspace(0, n_elements, n_bins + 1).astype(int)
+    bins = [slice(bin_edges[i], bin_edges[i + 1]) for i in range(len(bin_edges) - 1)]
+    return bins
 
 
 def get_binscatter_objects(
@@ -52,12 +59,7 @@ def get_binscatter_objects(
         x_data = x_data[:, None]
     reg = linear_model.LinearRegression().fit(x_data, y_data)
     if bins is None:
-        bin_edges = np.linspace(0, len(y), n_bins + 1).astype(int)
-        assert len(bin_edges) == n_bins + 1
-        bins = [
-            slice(bin_edges[i], bin_edges[i + 1]) for i in range(len(bin_edges) - 1)
-        ]
-        assert len(bins) == n_bins
+        bins = _get_bins(len(y), n_bins)
 
     x_means = [np.mean(x_data[bin_]) for bin_ in bins]
     y_means = [np.mean(y_data[bin_]) for bin_ in bins]
@@ -77,6 +79,7 @@ def binscatter(
     recenter_y: bool = True,
     # TODO: make 'bins' consistent with functions in other libraries, as in pd.cut
     bins: Optional[Iterable[slice]] = None,
+    fit_reg: Optional[bool] = True,
 ) -> Tuple[List[float], List[float], float, float]:
     """
     :param self: matplotlib.axes.Axes object.
@@ -95,10 +98,12 @@ def binscatter(
     :param bins: Indices of each bin. By default, if you leave 'bins' as None,
         binscatter constructs equal sized bins;
         if you don't like that, use this parameter to construct your own.
-    :return:
+    :param fit_reg: Whether to plot a regression line.
     """
     if line_kwargs is None:
         line_kwargs = {}
+    elif not fit_reg:
+        warnings.warn("Both fit_reg=False and non-None line_kwargs were passed.")
     if scatter_kwargs is None:
         scatter_kwargs = {}
 
@@ -108,12 +113,13 @@ def binscatter(
 
     self.scatter(x_means, y_means, **scatter_kwargs)
     x_range = np.array(self.get_xlim())
-    self.plot(
-        x_range,
-        intercept + x_range * coef,
-        label="beta=" + str(round(coef, 3)),
-        **line_kwargs
-    )
+    if fit_reg:
+        self.plot(
+            x_range,
+            intercept + x_range * coef,
+            label="beta=" + str(round(coef, 3)),
+            **line_kwargs
+        )
     # If series were passed, might be able to label
     if hasattr(x, "name"):
         self.set_xlabel(x.name)
